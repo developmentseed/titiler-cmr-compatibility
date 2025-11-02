53fb88498ad36e0b5d211841bd48eeacd329fc4e
Aimee Barciauskas <aimee@developmentseed.org>
2025-11-02 16:11:39 +0000

    Update mre

--- mre.py
+++ mre.py
@@ -1,30 +1,24 @@
 import earthaccess
-import logging
-
-from rio_tiler.io import Reader
 from titiler.cmr.backend import CMRBackend
 from titiler.cmr.reader import MultiFilesBandsReader
-logging.getLogger('earthaccess').setLevel(logging.DEBUG)
-
-auth = earthaccess.login() # using ~/.netrc
-s3_credentials = auth.get_s3_credentials(provider="LPCLOUD")
 
 cmr_query = {
     "concept_id": "C2021957295-LPCLOUD",
 }
+auth = earthaccess.login()
+
 tile_args = {
     "tile_x": 0,
     "tile_y": 0,
     "tile_z": 0,
     "cmr_query": cmr_query,
     "bands_regex": "B[0-9][0-9]",
-    "bands": ["B02"],
-    "s3_credentials": s3_credentials
+    "bands": ["B02"]
 }
 
 with CMRBackend(
     reader=MultiFilesBandsReader,
-    auth=auth,
+    auth=auth
 ) as src_dst:
     image, _ = src_dst.tile(**tile_args)
     print("successful tile operation")
